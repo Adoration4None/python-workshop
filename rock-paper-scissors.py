ba2ed0e58cba95888872f5358281fd16ae5b0982
Samuel <eb.samuel6@gmail.com>
2023-09-05 14:41:03 -0500

    Merge from Santiago and last details

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,18 +1,17 @@
-# import os
+import os
 from random import randint
 
-# ------------------------------ Movimientos -----------------------------------------
+# ------------------------------ Allowed Moves -----------------------------------------
 moves = {
     "0": "Piedra",
     "1": "Papel",
     "2": "Tijera"
 }
 
-# ------------------------------ Funciones -----------------------------------------
+# -------------------------------- Functions -----------------------------------------
 
-
-# def clearScreen():
-#     os.system("cls" if os.name == "nt" else "clear")
+def clearScreen():
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 def validChoice(playerChoice, moves):
@@ -20,9 +19,9 @@ def validChoice(playerChoice, moves):
 
 
 def playerPlays():
-    print("\n0: Piedra \U0000270A")  # ✊
-    print("1: Papel \U0000270B")  # ✋
-    print("2: Tijera \U0000270C")  # ✌️
+    print("\n0: Piedra \U0000270A") # ✊
+    print("1: Papel \U0000270B")    # ✋
+    print("2: Tijera \U0000270C")   # ✌️
 
     return input("Ingresa tu elección [0/1/2]: ")
 
@@ -46,9 +45,10 @@ def evaluate(playerChoice, machineChoice):
 
 
 def menu():
+    print("------------------------- PIEDRA, PAPEL O TIJERA ------------------------")
+
     playing = True
     while playing:
-        print("------------------------- PIEDRA, PAPEL O TIJERA ------------------------")
         playerChoice = playerPlays()
 
         while not validChoice(playerChoice, moves):
@@ -61,18 +61,14 @@ def menu():
         print(f"[Computador]: {moves[machineChoice]}")
 
         print(evaluate(playerChoice, machineChoice))
-        playing = input("\n¿Quieres jugar de nuevo? [s/n] ")
-        playing = True if playing == "s" else print("Gracias por jugar")
-        print("")
-
-        # clearScreen()
+        playing = input("\n¿Quieres jugar de nuevo? [s/n] ") == "s"
+        if not playing: print("Gracias por jugar")
 
 
 def run_game():
-    # clearScreen()
+    clearScreen()
     menu()
 
-
-# ------------------------------ Entrypoint -----------------------------------------
+# ----------------------------------- Entrypoint ---------------------------------------
 if __name__ == "__main__":
     run_game()
