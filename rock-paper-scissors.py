7375933054119d9db9c8e35b4ef9bf03105b4c4a
santi <tiagohig70@gmail.com>
2023-09-04 19:18:49 -0500

    Entrypoint creado

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,64 +1,76 @@
 import os
 from random import randint
 
+# ------------------------------ Movimientos -----------------------------------------
+moves = {
+    "0": "Piedra",
+    "1": "Papel",
+    "2": "Tijera"
+}
+
+# ------------------------------ Funciones -----------------------------------------
+
+
 def clearScreen():
     os.system("cls" if os.name == "nt" else "clear")
+
 
 def validChoice(playerChoice, moves):
     return playerChoice in moves.keys()
 
-def playerPlays():
-    print("\n0: Rock \U0000270A")  # ✊
-    print("1: Paper \U0000270B")  # ✋
-    print("2: Scissors \U0000270C")  # ✌️
 
-    return input("Enter your choice [0/1/2]: ")
+def playerPlays():
+    print("\n0: Piedra \U0000270A")  # ✊
+    print("1: Papel \U0000270B")  # ✋
+    print("2: Tijera \U0000270C")  # ✌️
+
+    return input("Ingresa tu elección [0/1/2]: ")
+
 
 def machinePlays():
-    return str( randint(0, 2) )
+    return str(randint(0, 2))
+
 
 def evaluate(playerChoice, machineChoice):
     if playerChoice == "0" and machineChoice == "2" or\
        playerChoice == "1" and machineChoice == "0" or\
        playerChoice == "2" and machineChoice == "1":
-        return "YOU WIN! :D"
-    
+        return "¡Ganaste! :D"
+
     if playerChoice == "2" and machineChoice == "0" or\
        playerChoice == "0" and machineChoice == "1" or\
        playerChoice == "1" and machineChoice == "2":
-        return "YOU LOSE :("
-    
-    return "IT'S A TIE!"
+        return "Perdiste :("
+
+    return "¡Es un empate!"
+
+# ------------------------------ Instrucciones Main -----------------------------------------
 
 
-# ------------------------------ Main instructions -----------------------------------------
-moves = {
-    "0": "Rock",
-    "1": "Paper",
-    "2": "Scissors"
-}
-
-clearScreen()
-print("------------------------- ROCK, PAPER, SCISSORS ------------------------")
-
-playing = True
-
-while playing:
-    playerChoice = playerPlays()
-    
-    while not validChoice(playerChoice, moves):
-        print("\nPlease enter a valid option [0/1/2]")
+def menu():
+    playing = True
+    while playing:
+        print("------------------------- PIEDRA, PAPEL O TIJERA ------------------------")
         playerChoice = playerPlays()
 
-    machineChoice = machinePlays()
+        while not validChoice(playerChoice, moves):
+            print("\nIngresa una opción válida, por favor [0/1/2]")
+            playerChoice = playerPlays()
 
-    print(f"\n[You]: {moves[playerChoice]}")
-    print(f"[Machine]: {moves[machineChoice]}")
+        machineChoice = machinePlays()
 
-    print( evaluate(playerChoice, machineChoice) )
-    playing = input("\nPlay again? [y/n] ") == "y"
+        print(f"\n[Tú]: {moves[playerChoice]}")
+        print(f"[Computador]: {moves[machineChoice]}")
+
+        print(evaluate(playerChoice, machineChoice))
+        playing = input("\n¿Quieres jugar de nuevo? [s/n] ") == "s"
+        clearScreen()
+
+
+def run_game():
     clearScreen()
+    menu()
 
 
-    
-
+if __name__ == "__main__":
+    run_game()
